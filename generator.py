b600847f0139da307e79643303398e8d42a3b0c5
JakubWilczek98 <jakubwilczek98@gmail.com>
2021-12-31 11:22:03 +0100

    Complete generator

--- generator.py
+++ generator.py
@@ -2,57 +2,54 @@ import random
 import string
 
 class Generate_password:
-    def __init__(self, letters_valu, digits_value, special_characters_value, pass_length): 
-        self.letters_valu = letters_valu
+    def __init__(self, letters_value, digits_value, special_characters_value, pass_length): 
+        if letters_value == "True":
+            letters_value = True
+        if digits_value == "True":
+            digits_value = True
+        if special_characters_value == "True":
+            special_characters_value = True
+        
+        self.letters_value = letters_value
         self.digits_value = digits_value
         self.special_characters_value = special_characters_value
         self.pass_length = pass_length
-    
-    
+
     def generate(self):
         #Letters
-        if letters_value is True and digits_value is False and special_characters_value is False:
+        if self.letters_value is True and self.digits_value is False and self.special_characters_value is False:
             generated_password = string.ascii_letters
         #Digits
-        elif letters_value is False and digits_value is True and special_characters_value is False:
+        elif self.letters_value is False and self.digits_value is True and self.special_characters_value is False:
             generated_password = string.digits
         #Special Characters
-        elif letters_value is False and digits_value is False and special_characters_value is True:
+        elif self.letters_value is False and self.digits_value is False and self.special_characters_value is True:
             generated_password = string.punctuation
          
         #Letters and digits
-        elif letters_value is True and digits_value is True and special_characters_value is False:
+        elif self.letters_value is True and self.digits_value is True and self.special_characters_value is False:
             generated_password = string.ascii_letters + string.digits
          
         #Letters and special characters
-        elif letters_value is True and digits_value is False and special_characters_value is True:
+        elif self.letters_value is True and self.digits_value is False and self.special_characters_value is True:
             generated_password = string.ascii_letters + string.punctuation
          
         #Digits and special charakters
-        elif letters_value is False and digits_value is True and special_characters_value is True:
+        elif self.letters_value is False and self.digits_value is True and self.special_characters_value is True:
             generated_password = string.digits + string.punctuation
             
         #Letters, digits and special charakters
-        elif letters_value is True and digits_value is True and special_characters_value is True:
-            generated_password = string.ascii_letters + string.digits + string.punctuation
-         
-        elif letters_value is False and digits_value is False and special_characters_value is False:
+        elif self.letters_value is True and self.digits_value is True and self.special_characters_value is True:
+            generated_password = string.ascii_letters + string.digits + string.punctuation         
+        #None length
+        elif self.pass_length == 0:
+            generated_password = "You have to choose password length"
+            return generated_password
+        #None type
+        elif self.letters_value is False and self.digits_value is False and self.special_characters_value is False:
             generated_password = "You have to choose at least one type"
             return generated_password
         
-        return "".join(random.choice(generated_password) for i in range(pass_length))
-
-    
-
-if __name__ == "__main__":
-    #letters, digits, special characters, letters_digits, letters_special_charakters, digits_special_charakters, letters_digits_special_charakters
-    
-    letters_value = True 
-    digits_value = True
-    special_characters_value = True
-    pass_length = 10
-    
-    password = Generate_password(letters_value, digits_value, special_characters_value, pass_length)
-    generated_password = password.generate()
-    print(generated_password)
-
+        
+        self.generated_password = "".join(random.choice(generated_password) for i in range(self.pass_length))
+        return self.generated_password
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from generator import Generate_password
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,12 +11,16 @@ def home():
 
 @app.route("/result", methods=["POST", "GET"])
 def result():
+    
+    output = request.form.to_dict()
     letters_value = False 
     digits_value = False
     special_characters_value = False
-    
-    output = request.form.to_dict()
-    
+    pass_length = output["pass_length"]
+    if pass_length == "":
+        pass_length = 0
+    else: 
+        pass_length = round(float(output["pass_length"]))
     
     if 'letters_value' in output:
         letters_value = output["letters_value"]
@@ -24,13 +29,14 @@ def result():
     if 'special_characters_value' in output:
         special_characters_value = output["special_characters_value"]
     
+    password = Generate_password(letters_value, digits_value, special_characters_value, pass_length)
+    new_password = password.generate()
     
-    return render_template("index.html", 
-                           letters_value = letters_value,
-                           digits_value = digits_value,
-                           special_characters_value = special_characters_value
+    
+    return render_template("results.html", 
+                           new_password = new_password
                            )
-    
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
